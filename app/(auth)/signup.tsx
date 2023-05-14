bc70763a6a92cf603586a1a3595c758fa0f84e57
roymandev <royman.dev@gmail.com>
2023-05-14 23:49:37 +0700

    feat: integrate signup function

--- app/(auth)/signup.tsx
+++ app/(auth)/signup.tsx
@@ -12,11 +12,46 @@ import { useState } from "react";
 import { Feather } from "@expo/vector-icons";
 import { Link, useRouter } from "expo-router";
 import IconGoogle from "../../assets/icons/google.svg";
+import { supabase } from "../../utils/supabase";
 
 const SignUpPage = () => {
   const router = useRouter();
   const theme = useTheme();
   const [showPassword, setShowPassword] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [acceptTerms, setAcceptTerms] = useState(false);
+
+  const signUpHanlder = async () => {
+    setLoading(true);
+
+    const { error } = await supabase.auth.signUp({
+      email,
+      password,
+      options: {
+        data: {
+          name,
+        },
+      },
+    });
+
+    if (error) {
+      setError(error.message);
+    } else {
+      router.push({
+        pathname: "/verification",
+        params: {
+          email,
+        },
+      });
+    }
+
+    setLoading(false);
+  };
 
   return (
     <>
@@ -24,11 +59,17 @@ const SignUpPage = () => {
 
       <ScrollView px={4}>
         <FormControl mt="56px">
-          <Input placeholder="Name" />
+          <Input placeholder="Name" value={name} onChangeText={setName} />
         </FormControl>
 
         <FormControl mt={6}>
-          <Input placeholder="Email" keyboardType="email-address" />
+          <Input
+            placeholder="Email"
+            keyboardType="email-address"
+            autoCapitalize="none"
+            value={email}
+            onChangeText={setEmail}
+          />
         </FormControl>
 
         <FormControl mt={6}>
@@ -51,15 +92,19 @@ const SignUpPage = () => {
                 )}
               </Button>
             }
+            value={password}
+            onChangeText={setPassword}
           />
         </FormControl>
 
         <FormControl mt={4}>
           <Checkbox
             value="agree"
+            isChecked={acceptTerms}
+            onChange={setAcceptTerms}
             accessibilityLabel="By signing up, you agree to the Terms of Service and Privacy Policy"
           >
-            <Text _android={{ pr: 5 }} _ios={{ pr: 5 }} lineHeight="md">
+            <Text _android={{ pr: 9 }} _ios={{ pr: 9 }} lineHeight="md">
               By signing up, you agree to the{" "}
               <Link href="/tos" style={{ color: theme.colors.primary[100] }}>
                 Terms of Service and Privacy Policy
@@ -68,7 +113,18 @@ const SignUpPage = () => {
           </Checkbox>
         </FormControl>
 
-        <Button mt={7} onPress={() => router.push("/verification")}>
+        {error && (
+          <Text mt={3} textAlign="center" color="red.500">
+            {error}
+          </Text>
+        )}
+
+        <Button
+          mt={7}
+          onPress={signUpHanlder}
+          isLoading={loading}
+          isDisabled={!name || !email || !password || !acceptTerms}
+        >
           Sign Up
         </Button>
         <Text textAlign="center" my={3} fontWeight="bold" color="light.20">
